623fdcde546cc08b5d882299d5f3fea7491c7410
George Nairooze <george.nairooze@linkdev.com>
2022-12-24 16:59:40 +0200

    combine screen capture

--- code/autocapture/AutoCaptureForm/frmCapture.Designer.cs
+++ code/autocapture/AutoCaptureForm/frmCapture.Designer.cs
@@ -35,6 +35,13 @@
             this.txtPath = new System.Windows.Forms.TextBox();
             this.numCaptureInterval = new System.Windows.Forms.NumericUpDown();
             this.lblCaptureInterval = new System.Windows.Forms.Label();
+            this.comboScreenTop = new System.Windows.Forms.ComboBox();
+            this.comboScreenBottom = new System.Windows.Forms.ComboBox();
+            this.comboScreenLeft = new System.Windows.Forms.ComboBox();
+            this.comboScreenRight = new System.Windows.Forms.ComboBox();
+            this.lblScreens = new System.Windows.Forms.Label();
+            this.radioVertical = new System.Windows.Forms.RadioButton();
+            this.radioHorizontal = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.numCaptureInterval)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,14 +49,14 @@
             // 
             this.lstLog.FormattingEnabled = true;
             this.lstLog.ItemHeight = 15;
-            this.lstLog.Location = new System.Drawing.Point(12, 116);
+            this.lstLog.Location = new System.Drawing.Point(278, 10);
             this.lstLog.Name = "lstLog";
             this.lstLog.Size = new System.Drawing.Size(412, 319);
             this.lstLog.TabIndex = 1;
             // 
             // btnStartCapture
             // 
-            this.btnStartCapture.Location = new System.Drawing.Point(14, 87);
+            this.btnStartCapture.Location = new System.Drawing.Point(14, 304);
             this.btnStartCapture.Name = "btnStartCapture";
             this.btnStartCapture.Size = new System.Drawing.Size(105, 23);
             this.btnStartCapture.TabIndex = 2;
@@ -59,7 +66,7 @@
             // 
             // btnStopCapture
             // 
-            this.btnStopCapture.Location = new System.Drawing.Point(319, 87);
+            this.btnStopCapture.Location = new System.Drawing.Point(157, 304);
             this.btnStopCapture.Name = "btnStopCapture";
             this.btnStopCapture.Size = new System.Drawing.Size(105, 23);
             this.btnStopCapture.TabIndex = 3;
@@ -80,7 +87,7 @@
             // 
             this.txtPath.Location = new System.Drawing.Point(51, 10);
             this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(373, 23);
+            this.txtPath.Size = new System.Drawing.Size(211, 23);
             this.txtPath.TabIndex = 5;
             this.txtPath.Text = "c:\\temp\\screenshots\\";
             // 
@@ -99,7 +106,7 @@
             0,
             65536});
             this.numCaptureInterval.Name = "numCaptureInterval";
-            this.numCaptureInterval.Size = new System.Drawing.Size(285, 23);
+            this.numCaptureInterval.Size = new System.Drawing.Size(123, 23);
             this.numCaptureInterval.TabIndex = 6;
             this.numCaptureInterval.ThousandsSeparator = true;
             this.numCaptureInterval.Value = new decimal(new int[] {
@@ -118,11 +125,82 @@
             this.lblCaptureInterval.TabIndex = 7;
             this.lblCaptureInterval.Text = "Capture Interval (sec)";
             // 
+            // comboScreenTop
+            // 
+            this.comboScreenTop.FormattingEnabled = true;
+            this.comboScreenTop.Location = new System.Drawing.Point(12, 149);
+            this.comboScreenTop.Name = "comboScreenTop";
+            this.comboScreenTop.Size = new System.Drawing.Size(121, 23);
+            this.comboScreenTop.TabIndex = 8;
+            // 
+            // comboScreenBottom
+            // 
+            this.comboScreenBottom.FormattingEnabled = true;
+            this.comboScreenBottom.Location = new System.Drawing.Point(12, 178);
+            this.comboScreenBottom.Name = "comboScreenBottom";
+            this.comboScreenBottom.Size = new System.Drawing.Size(121, 23);
+            this.comboScreenBottom.TabIndex = 9;
+            // 
+            // comboScreenLeft
+            // 
+            this.comboScreenLeft.FormattingEnabled = true;
+            this.comboScreenLeft.Location = new System.Drawing.Point(14, 243);
+            this.comboScreenLeft.Name = "comboScreenLeft";
+            this.comboScreenLeft.Size = new System.Drawing.Size(121, 23);
+            this.comboScreenLeft.TabIndex = 10;
+            // 
+            // comboScreenRight
+            // 
+            this.comboScreenRight.FormattingEnabled = true;
+            this.comboScreenRight.Location = new System.Drawing.Point(141, 243);
+            this.comboScreenRight.Name = "comboScreenRight";
+            this.comboScreenRight.Size = new System.Drawing.Size(121, 23);
+            this.comboScreenRight.TabIndex = 11;
+            // 
+            // lblScreens
+            // 
+            this.lblScreens.AutoSize = true;
+            this.lblScreens.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblScreens.Location = new System.Drawing.Point(14, 95);
+            this.lblScreens.Name = "lblScreens";
+            this.lblScreens.Size = new System.Drawing.Size(51, 15);
+            this.lblScreens.TabIndex = 16;
+            this.lblScreens.Text = "Screens";
+            // 
+            // radioVertical
+            // 
+            this.radioVertical.AutoSize = true;
+            this.radioVertical.Location = new System.Drawing.Point(10, 124);
+            this.radioVertical.Name = "radioVertical";
+            this.radioVertical.Size = new System.Drawing.Size(63, 19);
+            this.radioVertical.TabIndex = 17;
+            this.radioVertical.Text = "Vertical";
+            this.radioVertical.UseVisualStyleBackColor = true;
+            // 
+            // radioHorizontal
+            // 
+            this.radioHorizontal.AutoSize = true;
+            this.radioHorizontal.Checked = true;
+            this.radioHorizontal.Location = new System.Drawing.Point(13, 218);
+            this.radioHorizontal.Name = "radioHorizontal";
+            this.radioHorizontal.Size = new System.Drawing.Size(80, 19);
+            this.radioHorizontal.TabIndex = 18;
+            this.radioHorizontal.TabStop = true;
+            this.radioHorizontal.Text = "Horizontal";
+            this.radioHorizontal.UseVisualStyleBackColor = true;
+            // 
             // FrmCapture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(436, 450);
+            this.ClientSize = new System.Drawing.Size(702, 339);
+            this.Controls.Add(this.radioHorizontal);
+            this.Controls.Add(this.radioVertical);
+            this.Controls.Add(this.lblScreens);
+            this.Controls.Add(this.comboScreenRight);
+            this.Controls.Add(this.comboScreenLeft);
+            this.Controls.Add(this.comboScreenBottom);
+            this.Controls.Add(this.comboScreenTop);
             this.Controls.Add(this.lblCaptureInterval);
             this.Controls.Add(this.numCaptureInterval);
             this.Controls.Add(this.txtPath);
@@ -130,6 +208,7 @@
             this.Controls.Add(this.btnStopCapture);
             this.Controls.Add(this.btnStartCapture);
             this.Controls.Add(this.lstLog);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "FrmCapture";
             this.Text = "Auto-Capture";
@@ -148,5 +227,12 @@
         private TextBox txtPath;
         private NumericUpDown numCaptureInterval;
         private Label lblCaptureInterval;
+        private ComboBox comboScreenTop;
+        private ComboBox comboScreenBottom;
+        private ComboBox comboScreenLeft;
+        private ComboBox comboScreenRight;
+        private Label lblScreens;
+        private RadioButton radioVertical;
+        private RadioButton radioHorizontal;
     }
 }
--- code/autocapture/AutoCaptureForm/frmCapture.cs
+++ code/autocapture/AutoCaptureForm/frmCapture.cs
@@ -1,4 +1,5 @@
 using System.Drawing.Imaging;
+using System.Runtime.InteropServices.ObjectiveC;
 using System.Windows.Forms;
 using Timer = System.Windows.Forms.Timer;
 
@@ -16,6 +17,33 @@ namespace AutoCaptureForm
             captureTimer.Tick += CaptureTimer_Tick;
             captureTimer.Interval = 5000; //5 seconds
             capturePath = txtPath.Text;
+            this.Text = "Auto-Capture";
+
+            BindScreensControls();
+        }
+
+        private void BindScreensControls()
+        {
+            List<ScreenModel> screens = new ();
+            
+            for (int i = 0; i < Screen.AllScreens.Length; i++)
+            {
+                screens.Add(new ScreenModel() { Id = i, Name = $"Screen {i}" });
+            }
+
+            SetComboScreen(comboScreenTop, screens);
+            SetComboScreen(comboScreenBottom, screens);
+            SetComboScreen(comboScreenLeft, screens);
+            SetComboScreen(comboScreenRight, screens);
+        }
+
+        private static void SetComboScreen(ComboBox comboScreen, List<ScreenModel> screens)
+        {
+            comboScreen.Items.Clear();
+            for(int i = 0; i < screens.Count; i++)
+            {
+                comboScreen.Items.Add(screens[i].Name);
+            }
         }
 
         private void CaptureTimer_Tick(object? sender, EventArgs e)
@@ -23,22 +51,106 @@ namespace AutoCaptureForm
             try
             {
                 var captureDate = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-fff");
+                
+                Bitmap resultBitmap;
 
-                for (int i = 0; i < Screen.AllScreens.Length; i++)
+                if (radioHorizontal.Checked)
                 {
-                    var screen = Screen.AllScreens[i];
+                    if(comboScreenLeft.SelectedItem == null && comboScreenRight.SelectedItem == null)
+                    {
+                        AddLog("screen not set");
+                        return;
+                    }
 
-                    Bitmap captureBitmap = new(screen.Bounds.Width, screen.Bounds.Height, PixelFormat.Format32bppArgb);
-                    Graphics captureGraphics = Graphics.FromImage(captureBitmap);
-                    captureGraphics.CopyFromScreen(screen.Bounds.Left, screen.Bounds.Top, 0, 0, screen.Bounds.Size);
+                    if(comboScreenLeft.SelectedItem != null && comboScreenRight.SelectedItem == null)
+                    {
+                        int leftIndex = GetScreenIndexFromItem(comboScreenLeft.SelectedItem.ToString());
+                        Bitmap left = CaptureScreen(leftIndex);
 
-                    captureBitmap.Save($"{capturePath}Capture-{captureDate}-{i}.jpg", ImageFormat.Jpeg);
+                        left.Save($"{capturePath}Capture-{captureDate}.jpg", ImageFormat.Jpeg);
+                        left.Dispose();
 
-                    captureBitmap.Dispose();
-                    captureGraphics.Dispose();
+                        AddLog("screen captured");
+                        return;
+                    }
+
+                    if (comboScreenLeft.SelectedItem == null && comboScreenRight.SelectedItem != null)
+                    {
+                        int rightIndex = GetScreenIndexFromItem(comboScreenRight.SelectedItem.ToString());
+                        Bitmap right = CaptureScreen(rightIndex);
+
+                        right.Save($"{capturePath}Capture-{captureDate}.jpg", ImageFormat.Jpeg);
+                        right.Dispose();
+
+                        AddLog("screen captured");
+                        return;
+                    }
+
+                    if (comboScreenLeft.SelectedItem != null && comboScreenRight.SelectedItem != null)
+                    {
+                        int leftIndex = GetScreenIndexFromItem(comboScreenLeft.SelectedItem.ToString());
+                        Bitmap left = CaptureScreen(leftIndex);
+
+                        int rightIndex = GetScreenIndexFromItem(comboScreenRight.SelectedItem.ToString());
+                        Bitmap right = CaptureScreen(rightIndex);
+
+                        resultBitmap = CombineHorizontalBitmaps(left, right);
+
+                        resultBitmap.Save($"{capturePath}Capture-{captureDate}.jpg", ImageFormat.Jpeg);
+                        resultBitmap.Dispose();
+
+                        AddLog("screen captured");
+                        return;
+                    }
                 }
 
-                AddLog("screen captured");
+                if (radioVertical.Checked)
+                {
+                    if (comboScreenTop.SelectedItem == null && comboScreenBottom.SelectedItem == null)
+                    {
+                        AddLog("screen not set");
+                        return;
+                    }
+
+                    if (comboScreenTop.SelectedItem != null && comboScreenBottom.SelectedItem == null)
+                    {
+                        int topIndex = GetScreenIndexFromItem(comboScreenTop.SelectedItem.ToString());
+                        Bitmap top = CaptureScreen(topIndex);
+
+                        top.Save($"{capturePath}Capture-{captureDate}.jpg", ImageFormat.Jpeg);
+                        top.Dispose();
+
+                        AddLog("screen captured");
+                        return;
+                    }
+
+                    if (comboScreenTop.SelectedItem == null && comboScreenBottom.SelectedItem != null)
+                    {
+                        int bottomIndex = GetScreenIndexFromItem(comboScreenBottom.SelectedItem.ToString());
+                        Bitmap bottom = CaptureScreen(bottomIndex);
+
+                        bottom.Save($"{capturePath}Capture-{captureDate}.jpg", ImageFormat.Jpeg);
+                        bottom.Dispose();
+
+                        AddLog("screen captured");
+                        return;
+                    }
+
+                    if (comboScreenTop.SelectedItem != null && comboScreenBottom.SelectedItem != null)
+                    {
+                        int topIndex = GetScreenIndexFromItem(comboScreenTop.SelectedItem.ToString());
+                        Bitmap top = CaptureScreen(topIndex);
+                        int bottomIndex = GetScreenIndexFromItem(comboScreenBottom.SelectedItem.ToString());
+                        Bitmap bottom = CaptureScreen(bottomIndex);
+                        resultBitmap = CombineVerticalBitmaps(top, bottom);
+
+                        resultBitmap.Save($"{capturePath}Capture-{captureDate}.jpg", ImageFormat.Jpeg);
+                        resultBitmap.Dispose();
+
+                        AddLog("screen captured");
+                        return;
+                    }
+                }
             }
             catch (Exception ex)
             {
@@ -46,16 +158,72 @@ namespace AutoCaptureForm
             }
         }
 
+        private static int GetScreenIndexFromItem(string? screenName)
+        {
+            if (screenName == null)
+            {
+                return 0;
+            }
+
+            bool parseSucceeded = int.TryParse(screenName.Replace("Screen", string.Empty), out int screenIndex);
+
+            return (parseSucceeded)?screenIndex: 0;
+        }
+
+        private static Bitmap CombineHorizontalBitmaps(Bitmap left, Bitmap right)
+        {
+            Bitmap resultBitmap = new(left.Width + right.Width, left.Height, PixelFormat.Format32bppArgb);
+            Graphics resultGraphics = Graphics.FromImage(resultBitmap);
+            resultGraphics.DrawImage(left, 0, 0);
+            resultGraphics.DrawImage(right, left.Width, 0);
+
+            resultGraphics.Dispose();
+
+            return resultBitmap;
+        }
+
+        private static Bitmap CombineVerticalBitmaps(Bitmap top, Bitmap bottom)
+        {
+            Bitmap resultBitmap = new(top.Width, top.Height + bottom.Height, PixelFormat.Format32bppArgb);
+            Graphics resultGraphics = Graphics.FromImage(resultBitmap);
+            resultGraphics.DrawImage(top, 0, 0);
+            resultGraphics.DrawImage(bottom, 0, top.Height);
+
+            resultGraphics.Dispose();
+
+            return resultBitmap;
+        }
+        
+        private static Bitmap CaptureScreen(int screenIdex)
+        {
+            if (Screen.AllScreens.Length <= screenIdex)
+            {
+                screenIdex = 0;
+            }
+            
+            var screen = Screen.AllScreens[screenIdex];
+
+            Bitmap captureBitmap = new(screen.Bounds.Width, screen.Bounds.Height, PixelFormat.Format32bppArgb);
+            Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+            captureGraphics.CopyFromScreen(screen.Bounds.Left, screen.Bounds.Top, 0, 0, screen.Bounds.Size);
+
+            captureGraphics.Dispose();
+
+            return captureBitmap;
+        }
+
         private void BtnStartCapture_Click(object sender, EventArgs e)
         {
             captureTimer.Enabled = true;
             AddLog("capture started");
+            this.Text = "Auto-Capture - Started";
         }
 
         private void BtnStopCapture_Click(object sender, EventArgs e)
         {
             captureTimer.Enabled = false;
             AddLog("capture stopped");
+            this.Text = "Auto-Capture";
         }
 
         private void NumCaptureInterval_ValueChanged(object sender, EventArgs e)
